dfb3e492e6975d202e5a3282e747eb05594c78f8
Sagar Subedi <saagarsubedii999@gmail.com>
2025-11-26 23:22:55 +0545

    boolen more example updated

--- boolen/level1.js
+++ boolen/level1.js
@@ -8,5 +8,20 @@ const lang1 ='javascript';
 const lang2 = 'typescript';
 console.log(lang1.length === lang2.length);
 
+//check if num2 is less than or equal to num1 ,output should be true
+console.log(num2<= num1);
 
+//check if num1 is not equal to num2 ,output should be true
+console.log(num1!=num2);
 
+//check if num1 is equal to num2 ,output should be false
+console.log(num1 == num2);
+
+//check if both lang1 and lang2 are not equal ,output should be true (strictly not equal to)
+console.log(lang1 !== lang2);
+
+//using and operator ,check if num1 is greater than num2 and lang1 length is greater than lang2 length ,output should be false
+console.log((num1 > num2) && (lang1.length > lang2.length));
+
+//using or operator ,check if num2 is less than num1 or lang2 length is greater than lang1 length ,output should be true
+console.log((num2 < num1) || (lang2.length > lang1.length));
